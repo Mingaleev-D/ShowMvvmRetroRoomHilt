495e975dbbe840f4d80d14e5571cfa538290b206
admin <mingaleev.d92@mail.ru>
2023-10-16 06:55:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/showmvvmretroroomhilt/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.showmvvmretroroomhilt.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val White150 = White.copy(alpha = 0.15F)
+val White850 = White.copy(alpha = 0.85F)
+val Pink100 = Color(0xFFFFF1F1)
+val Pink900 = Color(0xFF3F2C2C)
+val Gray = Color(0xFF232323)
+val Green300 = Color(0xFFB8C9B8)
+val Green900 = Color(0xFF2D3B2D)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black500 = Color(0xff9a9b9d)
+val Black900 = Color(0xff191919)
---
+++ app/src/main/java/com/example/showmvvmretroroomhilt/ui/theme/Theme.kt
@@ -0,0 +1,52 @@
+package com.example.showmvvmretroroomhilt.ui.theme
+
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+@SuppressLint("ConflictingOnColor")
+private val DarkColorPalette = darkColors(
+    primary = Black900,
+    secondary = Black900,
+    primaryVariant = Black900,
+    background = Black900,
+    surface = White150,
+    onPrimary = White,
+    onSecondary = Black500,
+    onBackground = White,
+    onSurface = White850,
+)
+
+
+@SuppressLint("ConflictingOnColor")
+private val LightColorPalette = lightColors(
+    primary = White,
+    secondary = White,
+    primaryVariant = White,
+    background = White,
+    surface = White850,
+    onPrimary = Black900,
+    onSecondary = Black500,
+    onBackground = Gray,
+    onSurface = Gray,
+)
+
+@Composable
+fun ShowMvvmRetroRoomHiltTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+   val colors = if (darkTheme) {
+      DarkColorPalette
+   } else {
+      LightColorPalette
+   }
+
+   MaterialTheme(
+       colors = colors,
+       typography = Typography,
+       content = content
+   )
+}
